d4b6f8d5804358c65df9f74f9a8dc5aaf9061d7d
Ousmanbah10 <ousmanbah3903244@gmail.com>
2025-09-28 22:01:37 -0400

    New push

--- app.py
+++ app.py
@@ -24,6 +24,19 @@ if uploaded_file is not None:
         
     grid_size = 3
 
+     # Initialize session state
+    if "backend_matrix" not in st.session_state:
+        tiles, labels = split_image(image, grid_size)
+        labels[-1] = None  # blank tile
+        tiles[-1] = None
+        backend_matrix = [labels[i:i+grid_size] for i in range(0, len(labels), grid_size)]
+        
+        # Save both tile objects and base64-encoded versions
+        st.session_state.tiles = tiles
+        st.session_state.tile_b64 = [pil_to_base64(t) if t else None for t in tiles]
+        st.session_state.backend_matrix = backend_matrix
+  
+ 
     manual_play(grid_size)
     
     
